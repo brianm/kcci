034fb7f38dbf22d6a10035903e85db75df692d13
Brian McCallister <brianm@skife.org>
2023-03-31 11:19:07 -0700

    some stuff

--- book_finder.py
+++ book_finder.py
@@ -1,9 +1,9 @@
 from typing import List
 import requests
 
-'''
+"""
 For a given `(title, authors)` pair find the most likely candidate from Open Library.
-'''
+"""
 
 class Book:
     def __init__(self, title: str, authors: List[str], description: str) -> None:
